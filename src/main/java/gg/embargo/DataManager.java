d8531819d2ba915db67cba7884ef5fe900d56de1
Patrick <sharpienero@gmail.com>
2024-02-09 22:51:56 -0500

    Get rudementary wikisync functionality working

---
+++ src/main/java/gg/embargo/DataManager.java
@@ -0,0 +1,408 @@
+/*
+ * Copyright (c) 2021, andmcadams
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package gg.embargo;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import java.util.concurrent.TimeUnit;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.RuneScapeProfileType;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.Buffer;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+@Slf4j
+@Singleton
+public class DataManager
+{
+    @Inject
+    private Client client;
+
+    @Inject
+    private ClientThread clientThread;
+
+    @Inject
+    private OkHttpClient okHttpClient;
+
+    @Inject
+    private Gson gson;
+
+    @Inject
+    private EmbargoPlugin plugin;
+
+    private final HashMap<Integer, Integer> varbData = new HashMap<>();
+    private final HashMap<Integer, Integer> varpData = new HashMap<>();
+    private final HashMap<String, Integer> levelData = new HashMap<>();
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String MANIFEST_ENDPOINT = "https://9fba01f8-7689-4a3f-af1b-5e2445aa72c0.mock.pstmn.io/runelite/manifest";
+    private static final String VERSION_ENDPOINT = "https://8964a381-c461-455d-912a-0967c58d89a6.mock.pstmn.io/runelite/version";
+    private static final String POST_ENDPOINT = "https://8964a381-c461-455d-912a-0967c58d89a6.mock.pstmn.io/runelite/submit";
+
+    public void storeVarbitChanged(int varbIndex, int varbValue)
+    {
+        log.debug("Stored varb with index " + varbIndex + " and value " + varbValue);
+        synchronized (this)
+        {
+            varbData.put(varbIndex, varbValue);
+        }
+    }
+
+    public void storeVarbitChangedIfNotStored(int varbIndex, int varbValue)
+    {
+        log.debug("Attempting to store varb with index " + varbIndex + " and value " + varbValue);
+        synchronized (this)
+        {
+            if (!varbData.containsKey(varbIndex))
+                this.storeVarbitChanged(varbIndex, varbValue);
+        }
+    }
+
+    public void storeVarpChanged(int varpIndex, int varpValue)
+    {
+        log.debug("Stored varp with index " + varpIndex + " and value " + varpValue);
+        synchronized (this)
+        {
+            varpData.put(varpIndex, varpValue);
+        }
+    }
+
+    public void storeVarpChangedIfNotStored(int varpIndex, int varpValue)
+    {
+        log.debug("Attempting to store varp with index " + varpIndex + " and value " + varpValue);
+        synchronized (this)
+        {
+            if (!varpData.containsKey(varpIndex))
+                this.storeVarpChanged(varpIndex, varpValue);
+        }
+    }
+
+    public void storeSkillChanged(String skill, int skillLevel)
+    {
+        log.debug("Stored skill " + skill + " with level " + skillLevel);
+        synchronized (this)
+        {
+            levelData.put(skill, skillLevel);
+        }
+    }
+
+    public void storeSkillChangedIfNotChanged(String skill, int skillLevel)
+    {
+        log.debug("Attempting to store skill " + skill + " with level " + skillLevel);
+        synchronized (this)
+        {
+            if (!levelData.containsKey(skill))
+                storeSkillChanged(skill, skillLevel);
+        }
+    }
+
+    private <K, V> HashMap<K, V> clearChanges(HashMap<K, V> h)
+    {
+        HashMap<K, V> temp;
+        synchronized (this)
+        {
+            if (h.isEmpty())
+            {
+                return new HashMap<>();
+            }
+            temp = new HashMap<>(h);
+            h.clear();
+        }
+        return temp;
+    }
+
+    public void clearData()
+    {
+        synchronized (this)
+        {
+            varbData.clear();
+            varpData.clear();
+            levelData.clear();
+        }
+    }
+
+    private boolean hasDataToPush()
+    {
+        return !(varbData.isEmpty() && varpData.isEmpty() && levelData.isEmpty());
+    }
+
+    private JsonObject convertToJson()
+    {
+        JsonObject j = new JsonObject();
+        JsonObject parent = new JsonObject();
+        // We need to synchronize this to handle the case where the RuneScapeProfileType changes
+        synchronized (this)
+        {
+            RuneScapeProfileType r = RuneScapeProfileType.getCurrent(client);
+            HashMap<Integer, Integer> tempVarbData = clearChanges(varbData);
+            HashMap<Integer, Integer> tempVarpData = clearChanges(varpData);
+            HashMap<String, Integer> tempLevelData = clearChanges(levelData);
+
+            j.add("varb", gson.toJsonTree(tempVarbData));
+            j.add("varp", gson.toJsonTree(tempVarpData));
+            j.add("level", gson.toJsonTree(tempLevelData));
+
+            parent.addProperty("username", client.getLocalPlayer().getName());
+            parent.addProperty("profile", r.name());
+            parent.add("data", j);
+        }
+        log.debug(parent.toString());
+        return parent;
+    }
+
+    private void restoreData(JsonObject jObj)
+    {
+        synchronized (this)
+        {
+            if (!jObj.get("profile").getAsString().equals(RuneScapeProfileType.getCurrent(client).name()))
+            {
+                log.error("Not restoring data from failed call since the profile type has changed");
+                return;
+            }
+            JsonObject dataObj = jObj.getAsJsonObject("data");
+            JsonObject varbObj = dataObj.getAsJsonObject("varb");
+            JsonObject varpObj = dataObj.getAsJsonObject("varp");
+            JsonObject levelObj = dataObj.getAsJsonObject("level");
+            for (String k : varbObj.keySet())
+            {
+                this.storeVarbitChangedIfNotStored(Integer.parseInt(k), varbObj.get(k).getAsInt());
+            }
+            for (String k : varpObj.keySet())
+            {
+                this.storeVarpChangedIfNotStored(Integer.parseInt(k), varpObj.get(k).getAsInt());
+            }
+            for (String k : levelObj.keySet())
+            {
+                this.storeSkillChangedIfNotChanged(k, levelObj.get(k).getAsInt());
+            }
+        }
+    }
+
+    protected void submitToAPI()
+    {
+        if (!hasDataToPush() || client.getLocalPlayer() == null || client.getLocalPlayer().getName() == null)
+            return;
+
+        if (RuneScapeProfileType.getCurrent(client) == RuneScapeProfileType.BETA)
+            return;
+
+        log.debug("Submitting changed data to endpoint...");
+        JsonObject postRequestBody = convertToJson();
+        Request request = new Request.Builder()
+                .url(POST_ENDPOINT)
+                .post(RequestBody.create(JSON, postRequestBody.toString()))
+                .build();
+
+        OkHttpClient shortTimeoutClient = okHttpClient.newBuilder()
+                .callTimeout(5, TimeUnit.SECONDS)
+                .build();
+        try (Response response = shortTimeoutClient.newCall(request).execute())
+        {
+            if (!response.isSuccessful())
+            {
+                // If we failed to submit, readd the data to the data lists (unless there are newer ones)
+                log.error("Failed to submit data, attempting to reload dropped data...");
+                this.restoreData(postRequestBody);
+            }
+        }
+        catch (IOException ioException)
+        {
+            log.error("Failed to submit data, attempting to reload dropped data...");
+            this.restoreData(postRequestBody);
+        }
+    }
+
+    private HashSet<Integer> parseSet(JsonArray j)
+    {
+        HashSet<Integer> h = new HashSet<>();
+        for (JsonElement jObj : j)
+        {
+            h.add(jObj.getAsInt());
+        }
+        return h;
+    }
+
+    private static String bodyToString(final Request request){
+
+        try {
+            final Request copy = request.newBuilder().build();
+            final Buffer buffer = new Buffer();
+            copy.body().writeTo(buffer);
+            return buffer.readUtf8();
+        } catch (final IOException e) {
+            return "did not work";
+        }
+    }
+
+    protected void getManifest()
+    {
+        log.info("Getting manifest file...");
+        try
+        {
+            Request r = new Request.Builder()
+                    .url(MANIFEST_ENDPOINT)
+                    .build();
+            okHttpClient.newCall(r).enqueue(new Callback()
+            {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e)
+                {
+                    log.error("Error retrieving manifest", e);
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response)
+                {
+                    if (response.isSuccessful())
+                    {
+                        log.info("response.isSuccessful = True");
+                        try
+                        {
+                            // We want to be able to change the varbs and varps we get on the fly. To do so, we tell
+                            // the client what to send the server on startup via the manifest.
+                            if (response.body() == null)
+                            {
+                                log.info("Response.body() == null");
+                                log.error("Manifest request succeeded but returned empty body");
+                                response.close();
+                                return;
+                            }
+
+                            log.info("Response.body() != null");
+                            JsonObject j = new Gson().fromJson(response.body().string(), JsonObject.class);
+                            log.info(j.toString());
+
+                            try
+                            {
+                                log.info("Inside of try block");
+                                plugin.setVarbitsToCheck(parseSet(j.getAsJsonArray("varbits")));
+                                plugin.setVarpsToCheck(parseSet(j.getAsJsonArray("varps")));
+                                log.info("After plugin.set");
+                                try
+                                {
+                                    // Maybe this function should be run synch and this should be done outside of this
+                                    int manifestVersion = j.get("version").getAsInt();
+                                    if (plugin.getLastManifestVersion() != manifestVersion)
+                                    {
+                                        plugin.setLastManifestVersion(manifestVersion);
+                                        clientThread.invoke(() -> {
+                                            plugin.loadInitialData();
+                                        });
+                                    }
+                                }
+                                catch (UnsupportedOperationException | NullPointerException exception)
+                                {
+                                    plugin.setLastManifestVersion(-1);
+                                }
+                            }
+                            catch (NullPointerException e) {
+                                // This is probably an issue with the server. "varbits" or "varps" might be missing.
+                                log.error("Manifest possibly missing varbits or varps entry from /manifest call");
+                                log.error(e.getLocalizedMessage());
+                            }
+                            catch (ClassCastException e) {
+                                // This is probably an issue with the server. "varbits" or "varps" might be not be a list.
+                                log.error("Manifest from /manifest call might have varbits or varps as not a list");
+                                log.error(e.getLocalizedMessage());
+                            }
+                        }
+                        catch (IOException | JsonSyntaxException e)
+                        {
+                            log.error(e.getLocalizedMessage());
+                        }
+                    }
+                    else
+                    {
+                        log.error("Manifest request returned with status " + response.code());
+                        if (response.body() == null)
+                        {
+                            log.error("Manifest request returned empty body");
+                        }
+                        else
+                        {
+                            log.error(response.body().toString());
+                        }
+                    }
+                    response.close();
+                }
+            });
+        }
+        catch (IllegalArgumentException e)
+        {
+            log.error("Bad URL given: " + e.getLocalizedMessage());
+        }
+    }
+
+    protected int getVersion()
+    {
+        log.debug("Attempting to get manifest version...");
+        Request request = new Request.Builder()
+                .url(VERSION_ENDPOINT)
+                .build();
+
+        try (Response response = okHttpClient.newCall(request).execute())
+        {
+            if (!response.isSuccessful())
+            {
+                log.error("Failed to grab manifest version...");
+            }
+            else
+            {
+                try {
+                    JsonObject jObj = new Gson().fromJson(response.body().string(), JsonObject.class);
+                    log.debug("Found manifest version " + jObj.getAsJsonPrimitive("version").getAsInt());
+                    return jObj.getAsJsonPrimitive("version").getAsInt();
+                }
+                catch (IOException | NullPointerException exception)
+                {
+                    log.error("Failed to parse manifest version...");
+                }
+            }
+        }
+        catch (IOException ioException)
+        {
+            log.error("Failed to grab manifest version...");
+        }
+        return -1;
+    }
+
+}
--- src/main/java/gg/embargo/EmbargoPlugin.java
+++ src/main/java/gg/embargo/EmbargoPlugin.java
@@ -1,28 +1,37 @@
 package gg.embargo;
 
+import com.google.common.collect.HashMultimap;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.config.RuneScapeProfileType;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemStack;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.loottracker.LootReceived;
+import net.runelite.client.task.Schedule;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.http.api.loottracker.LootRecordType;
 import net.runelite.client.ui.ClientToolbar;
 
+import net.runelite.client.callback.ClientThread;
+
 import java.awt.image.BufferedImage;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
-import net.runelite.api.QuestState;
+import java.util.HashMap;
+import java.util.HashSet;
 
 @Slf4j
 @PluginDescriptor(
@@ -31,69 +40,227 @@ import net.runelite.api.QuestState;
 	tags = {"embargo", "clan", "embargo.gg", "ironman"}
 )
 public class EmbargoPlugin extends Plugin {
+
+	@Inject
+	private DataManager dataManager;
+
+	@Inject
+	private ClientThread clientThread;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private EmbargoConfig config;
 
-	private Collection<ItemStack> itemStacks;
-
 	@Getter
-	private EmbargoPanel panel;
+	@Setter
+	private int lastManifestVersion = -1;
 
-	@Inject
-	private ClientToolbar clientToolbar;
+	private int[] oldVarps;
+	private RuneScapeProfileType lastProfile;
 
-	private BufferedImage icon;
-	private NavigationButton navButton;
+	@Setter
+	private HashSet<Integer> varbitsToCheck;
 
-	@Override
-	protected void startUp() throws Exception {
-		log.info("Starting to build side panel");
-		buildSidePanel();
-		log.info("Done building side panel");
-		log.info("Embargo Clan started!");
+	@Setter
+	private HashSet<Integer> varpsToCheck;
 
+	private final HashMultimap<Integer, Integer> varpToVarbitMapping = HashMultimap.create();
+	private final HashMap<String, Integer> skillLevelCache = new HashMap<>();
+	private final int SECONDS_BETWEEN_UPLOADS = 10;
+	private final int SECONDS_BETWEEN_MANIFEST_CHECKS = 20*60;
+	private final int VARBITS_ARCHIVE_ID = 14;
 
-	}
-
-	@Override
-	protected void shutDown() throws Exception {
-		log.info("Embargo Clan stopped!");
-	}
-
-
-	@Subscribe
-	public void onGameStateChanged(final GameStateChanged event) {
-	}
-
-	String bossName;
-	@Subscribe
-	public void onLootReceived(final LootReceived event) {
-		if (event.getType() != LootRecordType.NPC && event.getType() != LootRecordType.EVENT) {
-			return;
-		}
-
-		log.info(event.toString());
-
-		itemStacks = event.getItems();
-		bossName = event.getName();
-
-		// Send to API for pointHistories
-	}
+	public static final String CONFIG_GROUP_KEY = "WikiSync";
+	// THIS VERSION SHOULD BE INCREMENTED EVERY RELEASE WHERE WE ADD A NEW TOGGLE
+	public static final int VERSION = 1;
 
 	@Provides
-	EmbargoConfig provideConfig(ConfigManager configManager) {
+	EmbargoConfig getConfig(ConfigManager configManager)
+	{
 		return configManager.getConfig(EmbargoConfig.class);
 	}
 
-	private void buildSidePanel() {
-		log.info("Inside of buildSidePanel");
-		panel = injector.getInstance(EmbargoPanel.class);
-		panel.sidePanelInitializer();
-		icon = ImageUtil.loadImageResource(getClass(), "/icon.png");
-		navButton = NavigationButton.builder().tooltip("Embargo Clan").icon(icon).priority(6).panel(panel).build();
-		clientToolbar.addNavigation(navButton);
+	@Override
+	protected void startUp() throws Exception
+	{
+		log.info("WikiSync started!");
+		setTogglesBasedOnVersion();
+		lastProfile = null;
+		varbitsToCheck = null;
+		varpsToCheck = null;
+		skillLevelCache.clear();
+		dataManager.getManifest();
 	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		log.info("WikiSync stopped!");
+		dataManager.clearData();
+	}
+
+	@Schedule(
+			period = SECONDS_BETWEEN_UPLOADS,
+			unit = ChronoUnit.SECONDS,
+			asynchronous = true
+	)
+	public void submitToAPI()
+	{
+		if (client != null && client.getGameState() != GameState.HOPPING)
+			dataManager.submitToAPI();
+	}
+
+	@Schedule(
+			period = SECONDS_BETWEEN_MANIFEST_CHECKS,
+			unit = ChronoUnit.SECONDS,
+			asynchronous = true
+	)
+	public void resyncManifest()
+	{
+		log.debug("Attempting to resync manifest");
+		if (dataManager.getVersion() != lastManifestVersion)
+		{
+			dataManager.getManifest();
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick gameTick)
+	{
+		log.info("aaaa");
+		// Call a helper function since it needs to be called from DataManager as well
+		checkProfileChange();
+	}
+
+	public void checkProfileChange()
+	{
+		RuneScapeProfileType r = RuneScapeProfileType.getCurrent(client);
+		if (r != lastProfile && client != null && varbitsToCheck != null && varpsToCheck != null)
+		{
+			// profile change, we should clear the datamanager and do a new initial dump
+			log.debug("Profile seemed to change... Reloading all data and updating profile");
+			lastProfile = r;
+			dataManager.clearData();
+			loadInitialData();
+		}
+	}
+
+	// Need to keep track of old varps and what varps each varb is in.
+	// On change
+	// Get varp, if varp in hashset, queue it.
+	// Get each varb index in varp. If varb changed and varb in hashset, queue it.
+	// Checking if varb has changed requires us to keep track of old varps
+	private void setupVarpTracking()
+	{
+		// Init stuff to keep track of varb changes
+		varpToVarbitMapping.clear();
+
+		if (oldVarps == null)
+		{
+			oldVarps = new int[client.getVarps().length];
+		}
+
+		// Set oldVarps to be the current varps
+		System.arraycopy(client.getVarps(), 0, oldVarps, 0, oldVarps.length);
+
+		// For all varbits, add their ids to the multimap with the varp index as their key
+		clientThread.invoke(() -> {
+			if (client.getIndexConfig() == null)
+			{
+				return false;
+			}
+			IndexDataBase indexVarbits = client.getIndexConfig();
+			final int[] varbitIds = indexVarbits.getFileIds(VARBITS_ARCHIVE_ID);
+			for (int id : varbitIds)
+			{
+				VarbitComposition varbit = client.getVarbit(id);
+				if (varbit != null)
+				{
+					varpToVarbitMapping.put(varbit.getIndex(), id);
+				}
+			}
+			return true;
+		});
+	}
+
+	public void loadInitialData()
+	{
+		for(int varbIndex : varbitsToCheck)
+		{
+			dataManager.storeVarbitChanged(varbIndex, client.getVarbitValue(varbIndex));
+		}
+
+		for(int varpIndex : varpsToCheck)
+		{
+			dataManager.storeVarpChanged(varpIndex, client.getVarpValue(varpIndex));
+		}
+		for(Skill s : Skill.values())
+		{
+			if (s != Skill.OVERALL)
+				dataManager.storeSkillChanged(s.getName(), client.getRealSkillLevel(s));
+		}
+	}
+
+	@Subscribe
+	public void onVarbitChanged(VarbitChanged varbitChanged)
+	{
+		if (client == null || varbitsToCheck == null || varpsToCheck == null)
+			return;
+		if (oldVarps == null)
+			setupVarpTracking();
+
+		int varpIndexChanged = varbitChanged.getIndex();
+		if (varpsToCheck.contains(varpIndexChanged))
+		{
+			dataManager.storeVarpChanged(varpIndexChanged, client.getVarpValue(varpIndexChanged));
+		}
+		for (Integer i : varpToVarbitMapping.get(varpIndexChanged))
+		{
+			if (!varbitsToCheck.contains(i))
+				continue;
+			// For each varbit index, see if it changed.
+			int oldValue = client.getVarbitValue(oldVarps, i);
+			int newValue = client.getVarbitValue(i);
+			if (oldValue != newValue)
+				dataManager.storeVarbitChanged(i, newValue);
+		}
+		oldVarps[varpIndexChanged] = client.getVarpValue(varpIndexChanged);
+	}
+
+	@Subscribe
+	public void onStatChanged(StatChanged statChanged)
+	{
+		if (statChanged.getSkill() == null || statChanged.getSkill() == Skill.OVERALL)
+			return;
+		Integer cachedLevel = skillLevelCache.get(statChanged.getSkill().getName());
+		if (cachedLevel == null || cachedLevel != statChanged.getLevel())
+		{
+			skillLevelCache.put(statChanged.getSkill().getName(), statChanged.getLevel());
+			dataManager.storeSkillChanged(statChanged.getSkill().getName(), statChanged.getLevel());
+		}
+	}
+
+	private void setTogglesBasedOnVersion()
+	{
+//		// Conditionally turn off certain features by default
+//		Integer version = configManager.getConfiguration(CONFIG_GROUP_KEY, WikiSyncConfig.WIKISYNC_VERSION_KEYNAME, Integer.class);
+//		if (version == null)
+//			return;
+//		int maxVersion = version;
+//		/* EXAMPLE TOGGLE SETTING CLAUSE */
+//		/* if (version < 2)
+//		{
+//			// Location tracking was added in deploy 2
+//			configManager.setConfiguration(CONFIG_GROUP_KEY, WikiSyncConfig.WIKISYNC_TOGGLE_KEYNAME, false);
+//			maxVersion = 2;
+//		}
+//		*/
+//
+//		// This is done here and not in each block because we don't want to rely on the order of the if clauses being correct.
+//		//configManager.setConfiguration(CONFIG_GROUP_KEY, WikiSyncConfig.WIKISYNC_VERSION_KEYNAME, maxVersion);
+//		log.debug("WikiSync version set to deployment number " + version);
+	}
+
 }
