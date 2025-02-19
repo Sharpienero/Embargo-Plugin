88fa4cdc73c25b6b7cae700295ab9b268577250d
Sharpienero <sharpienero@gmail.com>
2025-02-19 00:35:49 -0500

    Try to modify untrackableItemManager to use concurrency

--- src/main/java/gg/embargo/UntrackableItemManager.java
+++ src/main/java/gg/embargo/UntrackableItemManager.java
@@ -11,13 +11,16 @@ import okhttp3.*;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
 
 @Slf4j
 @Singleton
@@ -48,7 +51,6 @@ public class UntrackableItemManager {
         IMBUED_ZAMORAK_MAX_CAPE_I(24233),
         IMBUED_GUTHIX_MAX_CAPE_I(24234);
 
-
         private final int itemId;
 
         UntrackableItems(int itemId) {
@@ -67,28 +69,26 @@ public class UntrackableItemManager {
         }
         if (itemContainer != null && children != null) {
 
-            var itemMap = Arrays.stream(UntrackableItems.values()).map(UntrackableItems::getItemId).collect(Collectors.toCollection(HashSet::new));
-            List<Integer> playerItems = new ArrayList<>();
-            for (int i = 0; i < itemContainer.size(); ++i) {
+            var itemMap = Arrays.stream(UntrackableItems.values()).map(UntrackableItems::getItemId)
+                    .collect(Collectors.toCollection(HashSet::new));
+            List<Integer> playerItems = Arrays.stream(children)
+                    .map(Widget::getItemId)
+                    .filter(itemMap::contains)
+                    .collect(Collectors.toList());
 
-                Widget child = children[i];
-                var currentItem = child.getItemId();
-                if (itemMap.contains(currentItem)) {
-                    playerItems.add(currentItem);
-                }
-            }
+            final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-            var RequestBody = new FormBody.Builder();
-            for (int i=0; i < playerItems.size(); i++) {
-                RequestBody.add("itemIds[" + i + "]", String.valueOf(playerItems.get(i)));
-            }
+            JsonObject payload = new JsonObject();
+            payload.addProperty("username", username);
+            JsonArray items = new JsonArray();
+            playerItems.forEach(items::add);
+            payload.add("itemIds", items);
 
-            RequestBody.add("username", username);
+            RequestBody requestBody = RequestBody.create(JSON, payload.toString());
 
             Request request = new Request.Builder()
                     .url(UNTRACKABLE_ENDPOINT)
-                    .post(RequestBody.build())
-                    .addHeader("Content-Type", "application/json")
+                    .post(requestBody)
                     .build();
 
             try {
@@ -100,8 +100,14 @@ public class UntrackableItemManager {
 
                     @Override
                     public void onResponse(@NonNull Call call, @NonNull Response response) {
+                        handleResponse(response, "submit untrackable items");
+                    }
+
+                    private void handleResponse(Response response, String operation) {
                         if (response.isSuccessful()) {
-                            log.debug("Successfully submitted untrackable items");
+                            log.debug("Successfully {}", operation);
+                        } else {
+                            log.error("Failed to {} with status {}", operation, response.code());
                         }
                         response.close();
                     }
@@ -110,5 +116,77 @@ public class UntrackableItemManager {
                 log.error("Bad URL given: {}", e.getLocalizedMessage());
             }
         }
+    }
+
+    boolean isValidState(Widget widget, ItemContainer container) {
+        return widget != null && widget.getChildren() != null && container != null;
+    }
+
+    public void processPlayerBatch(List<String> usernames) {
+        JsonObject batchPayload = new JsonObject();
+        JsonArray players = new JsonArray();
+        usernames.forEach(username -> {
+            JsonObject playerData = new JsonObject();
+            playerData.addProperty("username", username);
+            playerData.add("items", getPlayerUntrackableItems(username));
+            players.add(playerData);
+        });
+        batchPayload.add("players", players);
+        submitBatch(batchPayload);
+    }
+
+    @Getter
+    private final MetricsCollector metrics = new MetricsCollector();
+
+    class MetricsCollector {
+        private final AtomicInteger successfulSubmissions = new AtomicInteger();
+        private final AtomicInteger failedSubmissions = new AtomicInteger();
+    }
+
+    private JsonArray getPlayerUntrackableItems(String username) {
+        Widget widget = client.getWidget(786445);
+        ItemContainer itemContainer = client.getItemContainer(InventoryID.BANK);
+
+        if (!isValidState(widget, itemContainer)) {
+            return new JsonArray();
+        }
+
+        var itemMap = Arrays.stream(UntrackableItems.values())
+                .map(UntrackableItems::getItemId)
+                .collect(Collectors.toCollection(HashSet::new));
+
+        JsonArray items = new JsonArray();
+        Arrays.stream(widget.getChildren())
+                .map(Widget::getItemId)
+                .filter(itemMap::contains)
+                .forEach(items::add);
+
+        return items;
+    }
+
+    private void submitBatch(JsonObject batchPayload) {
+        Request request = new Request.Builder()
+                .url(UNTRACKABLE_ENDPOINT)
+                .post(RequestBody.create(MediaType.parse("application/json"), batchPayload.toString()))
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                metrics.failedSubmissions.incrementAndGet();
+                log.error("Failed to submit batch of untrackable items", e);
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                metrics.successfulSubmissions.incrementAndGet();
+                if (response.isSuccessful()) {
+                    log.info("Successfully submitted batch of untrackable items");
+                } else {
+                    log.error("Failed to submit batch of untrackable items. Response code: " + response.code());
+                }
+                response.close();
+            }
+        });
     }
 }
