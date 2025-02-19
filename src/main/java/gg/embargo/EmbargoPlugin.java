b49a313f4113e69944b9140a8e52f788e8309696
Sharpienero <sharpienero@gmail.com>
2025-02-18 23:56:35 -0500

    Formatting

--- src/main/java/gg/embargo/EmbargoPlugin.java
+++ src/main/java/gg/embargo/EmbargoPlugin.java
@@ -32,11 +32,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
-@PluginDescriptor(
-	name = "Embargo Clan",
-	description = "A plugin to sync your account with Embargo",
-	tags = {"embargo", "clan", "embargo.gg", "ironman"}
-)
+@PluginDescriptor(name = "Embargo Clan", description = "A plugin to sync your account with Embargo", tags = { "embargo",
+		"clan", "embargo.gg", "ironman" })
 public class EmbargoPlugin extends Plugin {
 
 	@Inject
@@ -81,17 +78,16 @@ public class EmbargoPlugin extends Plugin {
 	private final HashMultimap<Integer, Integer> varpToVarbitMapping = HashMultimap.create();
 	private final HashMap<String, Integer> skillLevelCache = new HashMap<>();
 	private final int SECONDS_BETWEEN_UPLOADS = 30;
-	private final int SECONDS_BETWEEN_MANIFEST_CHECKS = 5*60;
+	private final int SECONDS_BETWEEN_MANIFEST_CHECKS = 5 * 60;
 	private final int VARBITS_ARCHIVE_ID = 14;
-	private static final Pattern COLLECTION_LOG_ITEM_REGEX = Pattern.compile("New item added to your collection log: (.*)");
+	private static final Pattern COLLECTION_LOG_ITEM_REGEX = Pattern
+			.compile("New item added to your collection log: (.*)");
 	private final HashMap<String, LocalDateTime> lastLootTime = new HashMap<>();
 	private final int SECONDS_BETWEEN_PROFILE_UPDATES = 15;
 	private final String CONFIG_GROUP = "embargo";
 
-
 	@Provides
-	EmbargoConfig getConfig(ConfigManager configManager)
-	{
+	EmbargoConfig getConfig(ConfigManager configManager) {
 		return configManager.getConfig(EmbargoConfig.class);
 	}
 
@@ -99,7 +95,7 @@ public class EmbargoPlugin extends Plugin {
 	protected void startUp() {
 		log.info("Embargo Clan plugin started!");
 
-		//Let's build out the side panels
+		// Let's build out the side panels
 		panel = injector.getInstance(EmbargoPanel.class);
 		panel.init();
 		final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "/icon.png");
@@ -138,14 +134,11 @@ public class EmbargoPlugin extends Plugin {
 		checkProfileChange();
 		noticeBoardManager.unsetNoticeBoard();
 	}
-	@Schedule(
-			period = SECONDS_BETWEEN_UPLOADS,
-			unit = ChronoUnit.SECONDS,
-			asynchronous = true
-	)
-	public void submitToAPI()
-	{
-		if (client != null && (client.getGameState() != GameState.HOPPING && client.getGameState() != GameState.LOGIN_SCREEN)) {
+
+	@Schedule(period = SECONDS_BETWEEN_UPLOADS, unit = ChronoUnit.SECONDS, asynchronous = true)
+	public void submitToAPI() {
+		if (client != null
+				&& (client.getGameState() != GameState.HOPPING && client.getGameState() != GameState.LOGIN_SCREEN)) {
 			dataManager.submitToAPI();
 			if (client.getLocalPlayer() != null) {
 				String username = client.getLocalPlayer().getName();
@@ -160,25 +153,15 @@ public class EmbargoPlugin extends Plugin {
 		}
 	}
 
-	@Schedule(
-			period = SECONDS_BETWEEN_MANIFEST_CHECKS,
-			unit = ChronoUnit.SECONDS,
-			asynchronous = true
-	)
-	public void resyncManifest()
-	{
+	@Schedule(period = SECONDS_BETWEEN_MANIFEST_CHECKS, unit = ChronoUnit.SECONDS, asynchronous = true)
+	public void resyncManifest() {
 		log.debug("Attempting to resync manifest");
-		if (dataManager.getVersion() != lastManifestVersion)
-		{
+		if (dataManager.getVersion() != lastManifestVersion) {
 			dataManager.getManifest();
 		}
 	}
 
-	@Schedule(
-		period = SECONDS_BETWEEN_PROFILE_UPDATES,
-		unit = ChronoUnit.SECONDS,
-		asynchronous = true
-	)
+	@Schedule(period = SECONDS_BETWEEN_PROFILE_UPDATES, unit = ChronoUnit.SECONDS, asynchronous = true)
 	public void checkProfileChanged() {
 		if (client.getLocalPlayer() != null && client.getGameState() == GameState.LOGGED_IN) {
 			panel.updateLoggedIn(true);
@@ -188,12 +171,12 @@ public class EmbargoPlugin extends Plugin {
 
 	@Getter
 	public enum MinigameCompletionMessages {
-		WINTERTODT("Your subdued Wintertodt count is:"), //g
+		WINTERTODT("Your subdued Wintertodt count is:"),
 		TEMPOROSS("Your Tempoross kill count is:"),
 		GOTR("Amount of rifts you have closed:"),
 		SOUL_WARS("team has defeated the Avatar"),
 		BARBARIAN_ASSAULT("Wave 10 duration"),
-		VOLCANIC_MINE("Your fragments disintegrate"); //g
+		VOLCANIC_MINE("Your fragments disintegrate");
 
 		private final String completionMessage;
 
@@ -220,43 +203,39 @@ public class EmbargoPlugin extends Plugin {
 	}
 
 	@Subscribe
-	public void onChatMessage(ChatMessage chatMessage)
-	{
+	public void onChatMessage(ChatMessage chatMessage) {
 		Player player = client.getLocalPlayer();
 
-		if (player == null)
-		{
+		if (player == null) {
 			return;
 		}
 
-		//Point History generation for new collection log
+		// Point History generation for new collection log
 		Matcher m = COLLECTION_LOG_ITEM_REGEX.matcher(chatMessage.getMessage());
 		RuneScapeProfileType profType = RuneScapeProfileType.getCurrent(client);
-		if (profType == RuneScapeProfileType.STANDARD && chatMessage.getType() == ChatMessageType.GAMEMESSAGE && m.matches()) {
+		if (profType == RuneScapeProfileType.STANDARD && chatMessage.getType() == ChatMessageType.GAMEMESSAGE
+				&& m.matches()) {
 			String obtainedItemName = Text.removeTags(m.group(1));
 			dataManager.uploadCollectionLogUnlock(obtainedItemName, player.getName());
 		}
 
-		if (profType == RuneScapeProfileType.STANDARD && (chatMessage.getType() == ChatMessageType.GAMEMESSAGE || chatMessage.getType() == ChatMessageType.FRIENDSCHATNOTIFICATION || chatMessage.getType() == ChatMessageType.SPAM))
-		{
+		if (profType == RuneScapeProfileType.STANDARD && (chatMessage.getType() == ChatMessageType.GAMEMESSAGE
+				|| chatMessage.getType() == ChatMessageType.FRIENDSCHATNOTIFICATION
+				|| chatMessage.getType() == ChatMessageType.SPAM)) {
 			String message = chatMessage.getMessage();
 			handleActivityCompletion(message);
 		}
 	}
 
 	public void handleActivityCompletion(String chatMessage) {
-		for (RaidCompletionMessages r : RaidCompletionMessages.values())
-		{
-			if (chatMessage.contains(r.getCompletionMessage()))
-			{
+		for (RaidCompletionMessages r : RaidCompletionMessages.values()) {
+			if (chatMessage.contains(r.getCompletionMessage())) {
 				dataManager.uploadRaidCompletion(r.name(), chatMessage);
 			}
 		}
 
-		for (MinigameCompletionMessages mg : MinigameCompletionMessages.values())
-		{
-			if (chatMessage.contains(mg.getCompletionMessage()))
-			{
+		for (MinigameCompletionMessages mg : MinigameCompletionMessages.values()) {
+			if (chatMessage.contains(mg.getCompletionMessage())) {
 				dataManager.uploadMinigameCompletion(mg.name(), chatMessage);
 			}
 		}
@@ -274,9 +253,11 @@ public class EmbargoPlugin extends Plugin {
 			return;
 		}
 
-		if (client != null && client.getLocalPlayer() != null && client.getLocalPlayer().getName() == null && event.getGameState() == GameState.LOGGED_IN) {
+		if (client != null && client.getLocalPlayer() != null && client.getLocalPlayer().getName() == null
+				&& event.getGameState() == GameState.LOGGED_IN) {
 			clientThread.invokeLater(() -> {
-				if (client.getLocalPlayer().getName() == null) return false;
+				if (client.getLocalPlayer().getName() == null)
+					return false;
 
 				panel.isLoggedIn = true;
 				panel.updateLoggedIn(true);
@@ -289,13 +270,13 @@ public class EmbargoPlugin extends Plugin {
 		}
 	}
 
-	public void checkProfileChange()
-	{
-		if (client == null) return;
+	public void checkProfileChange() {
+		if (client == null)
+			return;
 
 		RuneScapeProfileType r = RuneScapeProfileType.getCurrent(client);
-		if (r == RuneScapeProfileType.STANDARD && r != lastProfile && client != null && varbitsToCheck != null && varpsToCheck != null)
-		{
+		if (r == RuneScapeProfileType.STANDARD && r != lastProfile && client != null && varbitsToCheck != null
+				&& varpsToCheck != null) {
 			// profile change, we should clear the dataManager and do a new initial dump
 			log.debug("Profile seemed to change... Reloading all data and updating profile");
 			lastProfile = r;
@@ -309,32 +290,28 @@ public class EmbargoPlugin extends Plugin {
 	// Get varp, if varp in hashset, queue it.
 	// Get each varb index in varp. If varb changed and varb in hashset, queue it.
 	// Checking if varb has changed requires us to keep track of old varps
-	private void setupVarpTracking()
-	{
+	private void setupVarpTracking() {
 		// Init stuff to keep track of varb changes
 		varpToVarbitMapping.clear();
 
-		if (oldVarps == null)
-		{
+		if (oldVarps == null) {
 			oldVarps = new int[client.getVarps().length];
 		}
 
 		// Set oldVarps to be the current varps
 		System.arraycopy(client.getVarps(), 0, oldVarps, 0, oldVarps.length);
 
-		// For all varbits, add their ids to the multimap with the varp index as their key
+		// For all varbits, add their ids to the multimap with the varp index as their
+		// key
 		clientThread.invoke(() -> {
-			if (client.getIndexConfig() == null)
-			{
+			if (client.getIndexConfig() == null) {
 				return false;
 			}
 			IndexDataBase indexVarbits = client.getIndexConfig();
 			final int[] varbitIds = indexVarbits.getFileIds(VARBITS_ARCHIVE_ID);
-			for (int id : varbitIds)
-			{
+			for (int id : varbitIds) {
 				VarbitComposition varbit = client.getVarbit(id);
-				if (varbit != null)
-				{
+				if (varbit != null) {
 					varpToVarbitMapping.put(varbit.getIndex(), id);
 				}
 			}
@@ -342,38 +319,31 @@ public class EmbargoPlugin extends Plugin {
 		});
 	}
 
-	public void loadInitialData()
-	{
-		for (int varbIndex : varbitsToCheck)
-		{
+	public void loadInitialData() {
+		for (int varbIndex : varbitsToCheck) {
 			dataManager.storeVarbitChanged(varbIndex, client.getVarbitValue(varbIndex));
 		}
 
-		for (int varpIndex : varpsToCheck)
-		{
+		for (int varpIndex : varpsToCheck) {
 			dataManager.storeVarpChanged(varpIndex, client.getVarpValue(varpIndex));
 		}
-		for (Skill s : Skill.values())
-		{
+		for (Skill s : Skill.values()) {
 			dataManager.storeSkillChanged(s.getName(), client.getRealSkillLevel(s));
 		}
 	}
 
 	@Subscribe
-	public void onVarbitChanged(VarbitChanged varbitChanged)
-	{
+	public void onVarbitChanged(VarbitChanged varbitChanged) {
 		if (client == null || varbitsToCheck == null || varpsToCheck == null)
 			return;
 		if (oldVarps == null)
 			setupVarpTracking();
 
 		int varpIndexChanged = varbitChanged.getIndex();
-		if (varpsToCheck.contains(varpIndexChanged))
-		{
+		if (varpsToCheck.contains(varpIndexChanged)) {
 			dataManager.storeVarpChanged(varpIndexChanged, client.getVarpValue(varpIndexChanged));
 		}
-		for (Integer i : varpToVarbitMapping.get(varpIndexChanged))
-		{
+		for (Integer i : varpToVarbitMapping.get(varpIndexChanged)) {
 			if (!varbitsToCheck.contains(i))
 				continue;
 			// For each varbit index, see if it changed.
@@ -386,13 +356,11 @@ public class EmbargoPlugin extends Plugin {
 	}
 
 	@Subscribe
-	public void onStatChanged(StatChanged statChanged)
-	{
+	public void onStatChanged(StatChanged statChanged) {
 		if (statChanged.getSkill() == null)
 			return;
 		Integer cachedLevel = skillLevelCache.get(statChanged.getSkill().getName());
-		if (cachedLevel == null || cachedLevel != statChanged.getLevel())
-		{
+		if (cachedLevel == null || cachedLevel != statChanged.getLevel()) {
 			skillLevelCache.put(statChanged.getSkill().getName(), statChanged.getLevel());
 			dataManager.storeSkillChanged(statChanged.getSkill().getName(), statChanged.getLevel());
 		}
@@ -407,25 +375,23 @@ public class EmbargoPlugin extends Plugin {
 
 			var username = client.getLocalPlayer().getName();
 
-            if (lastLootTime.containsKey(username)) {
-                LocalDateTime lastLootTimestamp = lastLootTime.get(username);
+			if (lastLootTime.containsKey(username)) {
+				LocalDateTime lastLootTimestamp = lastLootTime.get(username);
 
-                if (LocalDateTime.now().isBefore(lastLootTimestamp)) {
-                    log.debug("Player has opened bank within the last 3 minutes, not checking for untrackable items");
-                    return;
-                }
+				if (LocalDateTime.now().isBefore(lastLootTimestamp)) {
+					log.debug("Player has opened bank within the last 3 minutes, not checking for untrackable items");
+					return;
+				}
 
-            }
-            untrackableItemManager.getUntrackableItems(username);
-            lastLootTime.put(username, LocalDateTime.now().plusMinutes(3));
-        }
+			}
+			untrackableItemManager.getUntrackableItems(username);
+			lastLootTime.put(username, LocalDateTime.now().plusMinutes(3));
+		}
 	}
 
 	@Subscribe
-	public void onLootReceived(final LootReceived event)
-	{
-		if (event.getType() != LootRecordType.NPC && event.getType() != LootRecordType.EVENT)
-		{
+	public void onLootReceived(final LootReceived event) {
+		if (event.getType() != LootRecordType.NPC && event.getType() != LootRecordType.EVENT) {
 			return;
 		}
 
@@ -438,13 +404,10 @@ public class EmbargoPlugin extends Plugin {
 	}
 
 	@Subscribe
-	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
-	{
-		clientThread.invokeLater(() ->
-		{
+	public void onWidgetLoaded(WidgetLoaded widgetLoaded) {
+		clientThread.invokeLater(() -> {
 			// TOB
-			if (widgetLoaded.getGroupId() == 364 || widgetLoaded.getGroupId() == 50)
-			{
+			if (widgetLoaded.getGroupId() == 364 || widgetLoaded.getGroupId() == 50) {
 				noticeBoardManager.setTOBNoticeBoard();
 			}
 
@@ -457,8 +420,7 @@ public class EmbargoPlugin extends Plugin {
 
 	@Subscribe
 	public void onConfigChanged(ConfigChanged event) {
-		if (!event.getGroup().equals(CONFIG_GROUP))
-		{
+		if (!event.getGroup().equals(CONFIG_GROUP)) {
 			return;
 		}
 
