cd2e17159a6c79c4b83ab743b205d2d1bbda413c
Sharpienero <sharpienero@protonmail.com>
2025-05-27 16:32:38 -0400

    Bump version to 1.4.8

--- src/main/java/gg/embargo/ui/EmbargoPanel.java
+++ src/main/java/gg/embargo/ui/EmbargoPanel.java
@@ -81,7 +81,7 @@ public class EmbargoPanel extends PluginPanel {
 
     void setupVersionPanel() {
         // Set up Embargo Clan Version at top of Version panel
-        JLabel version = new JLabel(htmlLabel("Embargo Clan Version: ", "1.4.6"));
+        JLabel version = new JLabel(htmlLabel("Embargo Clan Version: ", "1.4.8"));
         version.setFont(smallFont);
 
         // Set version's font
